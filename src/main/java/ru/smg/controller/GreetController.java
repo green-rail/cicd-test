f79eaeb644ed586963753c2ea9a4f204da8f05de
Evgenii Koduhov <zxjenya@yandex.ru>
2024-01-08 22:03:22 +0700

    init

---
+++ src/main/java/ru/smg/controller/GreetController.java
@@ -0,0 +1,13 @@
+package ru.smg.controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetController {
+    @GetMapping("/hello")
+    public String greet(ModelMap model) {
+        return "Hello";
+    }
+}
